7ce1f37c97d9410b238c9499a6ddd95fbafd771f
Abdul Hamid HmDi <axmxh@hotmail.com>
2019-08-27 04:07:40 +0200

    addTodoItme function

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ let todoController = (function() {
   let data = [];
 
   return {
-    newTodo: function(desc) {
+    addTodo: function(desc, status) {
       let id, newTodo;
-      if (data.length !== 0) {
+      if (data.length > 0) {
         id = data[data.length - 1].id + 1;
       } else {
         id = 0;
       }
-      newTodo = new Todo(id, desc, (status = 'toDo'));
+      newTodo = new Todo(id, desc, status);
       data.push(newTodo);
       return newTodo;
     },
@@ -28,14 +28,38 @@ let todoController = (function() {
 let UIController = (function() {
   let DOMStrings = {
     addTodo: '.add__todo',
-    addBtn: '.add__btn'
+    addBtn: '.add__btn',
+    todoList: '.todo__list'
   };
 
   return {
     getInput: function() {
       return {
-        description: document.querySelector(DOMStrings.addTodo).value
+        description: document.querySelector(DOMStrings.addTodo).value,
+        status: 'toDo'
       };
+    },
+    addTodoItem: function(obj) {
+      var html;
+      html = `<div class="item clearfix" id="todo-${obj.id}">
+      <input class="item__done" type="checkbox" />
+      <div class="item__description">${obj.description}</div>
+      <div class="right clearfix">
+        <div class="item__tag">${obj.status}</div>
+        <div class="item__edit">
+          <button class="item__edit--btn">
+            <i class="fas fa-edit"></i>
+          </button>
+        </div>
+        <div class="item__delete">
+          <button class="item__delete--btn">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </div>
+      </div>`;
+      document
+        .querySelector(DOMStrings.todoList)
+        .insertAdjacentHTML('beforeend', html);
     },
     getDomStrings: function() {
       return DOMStrings;
@@ -48,12 +72,14 @@ let controller = (function(todoCtrl, UICtrl) {
   document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddTodo);
 
   function ctrlAddTodo() {
-    let input;
+    let input, newTodo;
     // 1. get description fron input field
     input = UICtrl.getInput();
     if (input.description !== '') {
-      todoCtrl.newTodo(input.description);
+      // 2. add todo to todo ctrl
+      newTodo = todoCtrl.addTodo(input.description, input.status);
+      // 3. add todo to ui
+      UICtrl.addTodoItem(newTodo);
     }
-    //console.log(input.description);
   }
 })(todoController, UIController);
